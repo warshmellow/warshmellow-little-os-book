e9a96154eddea44e3639bbfea7ececb55dd2ea6e
warshmellow <ddwchensf@gmail.com>
2021-02-21 23:06:35 -0800

    Add extern kmain

--- loader.s
+++ loader.s
@@ -1,9 +1,17 @@
 global loader                     ; the entry symbol for ELF
+extern kmain
 
 MAGIC_NUMBER  equ 0x1BADB002      ; define the magic number constant
 FLAGS         equ 0x0             ; multiboot flags
 CHECKSUM      equ -MAGIC_NUMBER   ; calculate the checksum
                                   ; magic num + checksum + flags should be 0
+
+KERNEL_STACK_SIZE equ 4096
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
 
 section .text:                    ; start of the text
 align 4                           ; the code must be 4 byte aligned    
@@ -13,5 +21,7 @@ align 4                           ; the code must be 4 byte aligned
 
 loader:                           ; entry to linker script
     mov eax, 0xCAFECAFE           ; place number 0xCAFECAFE in the register EAX
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
+
 .loop:
     jmp .loop                     ; loop forever
